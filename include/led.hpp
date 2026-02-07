9042383e61178747ee36df51b9dae9f657682218
Michael Wu <michaeldmwu@gmail.com>
2026-02-06 20:18:25 -0500

    some function ideas

--- include/led.hpp
+++ include/led.hpp
@@ -22,5 +22,14 @@
 #include "example_ws2812.pio.h" // generated header
 
 // necessary functions
-void put_pixel();
-uint32_t make_rgb(uint8_t r, uint8_t g, uint8_t b);
+void put_pixel();                                   // writes rgb into strip buffer
+uint32_t make_rgb(uint8_t r, uint8_t g, uint8_t b); // converts r,g,b bytes into 32 bit format for ws2812
+void dma_init();                                    // init dma
+void dma_handler();                                 // An Interrupt Service Routine (ISR). When the DMA finishes sending data, this starts a 400µs timer. This "quiet time" tells the WS2812 LEDs to "latch" the data and show the colors.
+void output_strips_dma();                           // Updates the memory pointers for the next frame and triggers the DMA sequence.
+
+/*
+To drive multiple strips at once, the PIO needs one bit from every strip at the exact same time.
+*/
+
+void transform_strips(); // This converts standard 8-bit color values (0–255) into "bit planes." It calculates the brightness for each pixel and then "rotates" the data so that bit $N$ of all strips is stored together.
