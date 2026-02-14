ef66affd6ffa6695a7a468e390ec543c338d6a5d
Michael Wu <michaeldmwu@gmail.com>
2026-02-13 21:40:35 -0500

    dma + pio

--- include/led.hpp
+++ include/led.hpp
@@ -5,7 +5,6 @@
 #include <string.h>
 
 #include "pico/stdlib.h"
-#include "pico/sem.h"
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 #include "hardware/irq.h"
@@ -14,22 +13,71 @@
 #define NUM_PIXELS 64
 #define WS2812_PIN_BASE 2
 
-// Check the pin is compatible with the platform
 #if WS2812_PIN_BASE >= NUM_BANK0_GPIOS
 #error Attempting to use a pin>=32 on a platform that does not support it
 #endif
 
-#include "example_ws2812.pio.h" // generated header
+#include "example_ws2812.pio.h"
 
-// necessary functions
-void put_pixel();                                   // writes rgb into strip buffer
-uint32_t make_rgb(uint8_t r, uint8_t g, uint8_t b); // converts r,g,b bytes into 32 bit format for ws2812
-void dma_init();                                    // init dma
-void dma_handler();                                 // An Interrupt Service Routine (ISR). When the DMA finishes sending data, this starts a 400µs timer. This "quiet time" tells the WS2812 LEDs to "latch" the data and show the colors.
-void output_strips_dma();                           // Updates the memory pointers for the next frame and triggers the DMA sequence.
+typedef enum error
+{
+    PIO_INIT_ERROR,
+    DMA_INIT_ERROR,
+} error_code;
 
-/*
-To drive multiple strips at once, the PIO needs one bit from every strip at the exact same time.
-*/
+class led_strip
+{
+public:
+    led_strip(int in_pwr, int in_gnd, int in_data)
+    {
+        pwr_pin = in_pwr;
+        gnd_pin = in_gnd;
+        data_pin = in_data;
+    };
 
-void transform_strips(); // This converts standard 8-bit color values (0–255) into "bit planes." It calculates the brightness for each pixel and then "rotates" the data so that bit $N$ of all strips is stored together.
+    /// @brief init pio peripherial
+    void init_pio();
+
+    /// @brief timer for data latch
+    void init_timer();
+
+    /// @brief initializes dma for pio
+    void init_dma();
+
+    // TODO: IMPLEMENT VARIOUS PATTERNS
+
+    //
+
+    /// @brief converts r,g,b bytes into single 32 bit format
+    /// @param r
+    /// @param g
+    /// @param b
+    /// @return 32 package for ws2812
+    uint32_t make_rgb(uint8_t r, uint8_t g, uint8_t b);
+
+    /// @brief writes rgb into strip buffer
+    void put_pixel();
+
+    /// @brief converts 8 bit color values (0-255) into bit planes. To drive multiple strips at once, the PIO needs one bit from every strip at the exact same time.
+    void transform_strips();
+
+    /// @brief updates memory pointer for next frame (output every 1ms)
+    void output_strips_dma();
+
+    /// @brief wrapper for irq for outputting ws2812
+    static void output_wrapper();
+
+    PIO pio;
+    uint offset;
+    int state_machine;
+    int dma_channel;
+
+private:
+    static led_strip *instance;
+
+    int pwr_pin;
+    int gnd_pin;
+    int data_pin;
+
+    uint32_t rgb[20];
+};
