06f58642e14b0edb057740722298728085c1f2d9
Connor-Lai <connorlai@gmail.com>
2026-02-20 21:20:32 -0500

    configured UART properly and ran test from laptop to uart

--- src/main.c
+++ src/main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 #include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
-#include "hardware/irq.h"
 
-void init_uart();
+void init_uart_pins();
 
 int main()
 {
-    stdio_init_all();
+    
     init_uart_pins();
-    printf("hi");
-    return 0;
+    for (;;) {
+        char buf[2];
+        uart_read_blocking(uart0, (uint8_t*)buf, sizeof(buf) - 1);
+        buf[sizeof(buf) - 1] = '\0'; // Ensure null-termination
+        uart_puts(uart0, "You said: ");
+        uart_puts(uart0, buf);
+        uart_puts(uart0, "\n");
+    }
+    
 }
--- src/uart_comm.c
+++ src/uart_comm.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 #include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
-#include "hardware/irq.h"
 
-#define UART_ID uart1
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
+#define UART_ID0 uart0 //manager
+#define UART_TX0_PIN 0 
+#define UART_RX0_PIN 1
 #define BAUD_RATE 115200
+
+#define UART_ID1 uart1 //subordinate
+#define UART_TX1_PIN 36
+#define UART_RX1_PIN 37
+
+
+/*
+void uart_rx_isr()
+{
+    gpio_acknowledge_irq(5, UART_IRQ_NUM(UART_ID));
+    
+}
+*/
 
 void init_uart_pins()
 {
     
-    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID,UART_TX_PIN));
-    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID,UART_RX_PIN));
-    uart_init(UART_ID, BAUD_RATE);
-    uart_set_fifo_enabled(UART_ID, false);
-    irq_set_exclusive_handler(UART_IRQ_NUM(UART_ID), uart_rx_isr);
-    irq_set_enabled(UART_IRQ_NUM(UART_ID),true);
-    uart_set_irqs_enabled(UART_ID, true, false);
+    gpio_set_function(UART_TX0_PIN, UART_FUNCSEL_NUM(UART_ID0,UART_TX0_PIN));
+    gpio_set_function(UART_RX0_PIN, UART_FUNCSEL_NUM(UART_ID0,UART_RX0_PIN));
+    uart_init(UART_ID0, BAUD_RATE);
+    uart_set_fifo_enabled(UART_ID0, false);
+    
+    gpio_set_function(UART_TX1_PIN, UART_FUNCSEL_NUM(UART_ID1,UART_TX1_PIN));
+    gpio_set_function(UART_RX1_PIN, UART_FUNCSEL_NUM(UART_ID1,UART_RX1_PIN));
+    uart_init(UART_ID1, BAUD_RATE);
+    uart_set_fifo_enabled(UART_ID1, false);
+    
+    //irq_set_exclusive_handler(UART_IRQ_NUM(UART_ID), uart_rx_isr);
+    //irq_set_enabled(UART_IRQ_NUM(UART_ID),true);
+    //uart_set_irqs_enabled(UART_ID, true, false);
 }   
 
-void uart_rx_isr()
-{
-    gpio_acknowledge_irq(5, UART)
-}
