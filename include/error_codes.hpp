ead00b734a55b273ab2aa8cbc60544a961189a50
Michael Wu <michaeldmwu@gmail.com>
2026-02-20 22:06:55 -0500

    s

--- include/error_codes.hpp
+++ include/error_codes.hpp
@@ -6,3 +6,15 @@ typedef enum error
     DMA_INIT_ERROR,
     // TODO: IMPLEMENT MORE ERROR CODES
 } error_code;
+
+typedef enum mode
+{
+    BREATHING,
+    COMET,
+    RAINBOW_CYCLE,
+    TRAVELING_RAINBOW,
+    RIPPLE,
+    COLUMN_FLASH,
+    HEAT_MAP,
+    SNAKE,
+} mode_type;
