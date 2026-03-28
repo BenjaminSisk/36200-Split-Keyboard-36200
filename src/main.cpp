db8c229485f12192d93d03cf505c5598e3c1a882
BenjaminSisk <siskb@purdue.edu>
2026-03-27 21:44:33 -0400

    add USB subsystem from benBranch work

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,40 @@
 
 #define DATAPIN0 25
 #define SPACING 10
-#define LEFT_OR_RIGHT 0 // left = 0, right = 1
 
-#if LEFT_OR_RIGHT == 0
-static auto key2led = Key2LED::loadFromJson("left_keymap.json");
-#else
-static auto key2led = Key2LED::loadFromJson("right_keymap.json");
-#endif
+struct Coord
+{
+    int row;
+    int col;
+};
+
+static std::map<int, Coord> keymap = {
+    // Row 0 (Numbers/Tilde)
+    {0, {0, 0}},
+    {1, {0, 1}},
+    {2, {0, 2}},
+    {3, {0, 3}},
+    {4, {0, 4}},
+    {5, {0, 5}},
+    // Row 1 (Tab / Q-T)
+    {6, {1, 0}},
+    {7, {1, 1}},
+    {8, {1, 2}},
+    {9, {1, 3}},
+    {10, {1, 4}},
+    {11, {1, 5}},
+    // Row 2 (A-G)
+    {12, {2, 1}},
+    {13, {2, 2}},
+    {14, {2, 3}},
+    {15, {2, 4}},
+    {16, {2, 5}},
+    // Row 3 (Z-B)
+    {17, {3, 1}},
+    {18, {3, 2}},
+    {19, {3, 3}},
+    {20, {3, 4}},
+    {21, {3, 5}}};
 
 void main1()
 {
@@ -40,13 +67,12 @@ void main1()
     {
         if (multicore_fifo_rvalid())
         {
+
             uint32_t key_press = multicore_fifo_pop_blocking();
-            uint8_t id = (key_press >> 8) & 0xff;
-            // uint8_t action = (key_press) & 0xff;
             struct Coord pos = {-1, -1};
-            if (key2led.count(id))
+            if (keymap.count(key_press))
             {
-                pos = key2led[id];
+                pos = keymap[key_press];
             }
             strips[0].x = pos.col;
             strips[0].y = pos.row;
@@ -64,12 +90,11 @@ void main1()
 int main()
 {
     stdio_init_all();
-    sleep_ms(500);
+    init_uart_pins();
+    init_uart_isr();
     multicore_launch_core1(main1);
 
     adc_init();
-    init_uart_pins();
-    init_uart_isr();
 
     printf("\033[2J");
     printf("System Initialized. Starting IRQs...\n");
